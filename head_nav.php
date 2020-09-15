ee823327577942293c21dc1a9b16bf6f4d6350dd
Iszac <58008716+jarvisi71729@users.noreply.github.com>
2020-09-15 14:47:42 +1200

    15/09/2020
    munky-chan

---
+++ head_nav.php
@@ -0,0 +1,54 @@
+<!DOCTYPE HTML>
+
+<html lang="en">
+
+<?php
+
+    session_start();
+    include("config.php");
+    
+    // Connect to database...
+    
+    $dbconnect=mysqli_connect(DB_HOST, DB_USERNAME, DB_PASSWORD, DB_NAME);
+    
+    if (mysqli_connect_errno())
+    
+    {
+        echo "Connection failed:".mysqli_connect_error();
+        exit;
+        
+    }
+    
+?>
+    
+<head>
+    <meta charset="UTF-8">
+    <meta name="description" content="Put Content Here">
+    <meta name="keywords" content="Put keywords here">
+    <meta name="author" content="Put your name here">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   
+    <title>Food Review Database</title>
+
+    <link href="https://fonts.googleapis.com/css?family=Lato%7cUbuntu" rel="stylesheet">
+   
+    <link rel="stylesheet" href="css/font-awesome.min.css">
+    <link rel="stylesheet" href="foo.css">
+   
+</head>
+   
+<body>
+   
+    <div class="wrapper">
+   
+        <a href="index.php">
+            <div class="box logo">
+           
+            </div>    <!-- / logo -->
+        </a>
+       
+        <div class="box banner">
+            
+            <h1 style="font-size: 55px">Food Reviews</h1>
+
+        </div>    <!-- / banner -->
--- index.php
+++ index.php
@@ -1,77 +1,29 @@
 <!DOCTYPE HTML>
 
-<html lang="en">
+<?php 
+    include "head_nav.php";
+?>
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="description" content="Put Content Here">
-    <meta name="keywords" content="Put keywords here">
-    <meta name="author" content="Put your name here">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    
-    <title>Website Title goes here</title>
-    
-    <!-- Edit the link below / replace with your chosen google font -->
-    <link href="https://fonts.googleapis.com/css?family=Lato%7cUbuntu" rel="stylesheet"> 
-    
-    <!-- Edit the name of your style sheet - 'foo' is not a valid name!! -->
-    <link rel="stylesheet" href="css/foo.css"> 
-    
-</head>
-    
-<body>
-    
-    <div class="wrapper">
-    
-
-        
-        <div class="box banner">
-            
-        <!-- logo image linking to home page goes here -->
-        <a href="index.php">
-            <div class="box logo"  title="Logo - Click here to go to the Home Page">
-            <img class="img-circle" src="images/gen_logo.png" width="150" height="150" alt="generic logo" />
-            
-            </div>    <!-- / logo -->
-        </a>
-            
-            <h1>Website Name</h1>
-        </div>    <!-- / banner -->
-
-        <!-- Navigation goes here.  Edit BOTH the file name and the link name -->
-        <div class="box nav">
-            <a href="index.html">Home</a> | 
-            <a href="page1.html">Page 1</a> | 
-            <a href="page2.html">Page 2</a> | 
-            <a href="page3.html">Page 3</a> | 
-            <a href="contact.html">Contact</a>
-        </div>    <!-- / nav -->        
-        
-        
         <div class="box side">
-            Side Bar goes here
+            Database Search Bar
         </div>
         
         <div class="box main">
-            <h2>First Page heading</h2>
+            <h2>Iszac's Food Reviews</h2>
             
             <p>
-                Sugar plum powder dessert bonbon powder lollipop lollipop muffin I love. Tiramisu dessert I love sugar plum apple pie marzipan chocolate bar brownie. Pudding tiramisu caramels biscuit tiramisu. I love gummies gummies pie sweet pudding I love I love. Donut halvah candy oat cake donut. Caramels wafer danish jelly-o.
+                As someone who loves visiting resteraunts, I enjoy reviewing the foods I eat just as much. With today's digital age, many diners are referring to online resteraunt/food review websites as reading reviews are a great way to grasp the rating of a dish. It is said that 90% of diners research a resteraunt online before deciding where to eat which is why I have decided to make a database reviewing 10 different dishes I have eaten.
             </p>
             
             <p>
-                Jelly beans gingerbread jelly tootsie roll. Lemon drops bear claw I love tiramisu candy canes cake. Jelly-o apple pie powder tart I    love candy tiramisu. Topping liquorice cake sesame snaps brownie. Lemon drops jelly beans brownie danish I love fruitcake cotton candy danish. Dragée pastry halvah macaroon I love chocolate bar caramels liquorice. Halvah cupcake muffin fruitcake pastry I love bear claw I love jelly. Tart chocolate bar croissant danish jelly. Sugar plum powder cupcake muffin gingerbread I love sweet.
+                Please use the tools offered on the left side to filter or search any foods you are interested in and read the review with care.
             </p>
             
-            <p>
-                Biscuit jelly I love jelly-o candy canes chupa chups topping lemon drops I love. Sesame snaps macaroon lemon drops carrot cake I love. Chocolate bar gingerbread chupa chups. Dessert chocolate pastry liquorice tootsie roll lemon drops sweet macaroon liquorice. I love muffin bonbon. Cupcake apple pie chocolate cake bonbon topping. Macaroon gummi bears dragée caramels donut chocolate.
-            </p>
             
         </div>    <!-- / main -->
         
-
         <div class="box footer">
-            CC yourname 20XX
+            CC Iszac Jarvis 2020
         </div>    <!-- / footer -->
     
     </div>  <!-- / wrapper  -->
